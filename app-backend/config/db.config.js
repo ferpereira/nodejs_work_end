8982b256e5465415171cb51c60dafd266b311054
fernando_pereira+1-at-021171016440 <fernando.pereira@softtek.com>
2022-11-10 22:46:45 -0300

    criação do projeto e seus arquivos

---
+++ app-backend/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "ec2-54-159-175-38.compute-1.amazonaws.com",
+  USER: "cnjtgaernpqtnd",
+  PASSWORD: "c2838f80f59c045f88798e75ce418ffa74b8b78d94c4e5c1f26b8e7a30f71bd5",
+  DB: "dsq6kfn7uo7je",
+  dialect: "pg",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
